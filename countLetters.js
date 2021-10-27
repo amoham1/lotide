fc0c4da69a15cb90609dd813ec3be74dee53adef
amoham1 <amoham456@gmail.com>
2021-10-27 17:36:30 -0400

    add and solve countLetter

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  const pass = ` ✅✅✅  Assertion Passed: `;
+  const fail = ` 🛑🛑🛑  Assertion Failed: `
+  if (actual === expected) {
+    return console.log(`${pass}` + actual + " === " + expected);
+  } else {
+    return console.log(`${fail}` + actual + " !== " + expected);
+  }
+}; 
+
+const countLetters = function (string) {
+   let counter = {};
+  for (let letter of string) {
+    if (letter !== " ") {
+      if (counter[letter]) {
+        counter[letter] += 1;
+      }
+      else {
+        counter[letter] = 1;
+      }
+    }
+  }
+  return counter
+}
+console.log(countLetters("lighthouse in the house"))
--- countOnly.js
+++ countOnly.js
@@ -8,12 +8,10 @@ const assertEqual = function(actual, expected) {
     return console.log(`${fail}`+ actual + " !== " + expected);
   }
 };
-
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
-  const results = {};
-
+  const results = {}
   for (const item of allItems) {
     if (itemsToCount[item]) { 
       if (results[item]) {
@@ -23,10 +21,11 @@ const countOnly = function(allItems, itemsToCount) {
       }
     }
     console.log(item);
-  }
 
+  }
   return results;
 }
+
 
 const firstNames = [
   "Karl",
@@ -41,7 +40,6 @@ const firstNames = [
 ];
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
