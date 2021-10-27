17d3df419e6caa20724dc38c4f3af54f5a9cd9b4
amoham1 <amoham456@gmail.com>
2021-10-27 18:03:36 -0400

    added and solved letterPostion

---
+++ letterPositions.js
@@ -0,0 +1,24 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  let counter = -1;
+  // logic to update results here
+  for (letters of sentence){
+    if (letters !== " "){
+      if(results[letters]){
+        counter += 1;
+        results[letters].push(counter);
+      }
+      else{
+        counter += 1;
+        results[letters]= [counter]
+      }
+      
+    }
+    else {
+      counter += 1;
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"))
